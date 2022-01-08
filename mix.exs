bbeda1d03b50d64f2f4190886b0f5aca786e1f30
Rodrigo Azevedo <rodrigozcma@gmail.com>
2022-01-08 19:31:39 -0300

    docs: adding some dependencies

--- mix.exs
+++ mix.exs
@@ -49,7 +49,9 @@ defmodule BankUp.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:plug_cowboy, "~> 2.5"},
+      {:poison, "~> 5.0"}
     ]
   end
 
